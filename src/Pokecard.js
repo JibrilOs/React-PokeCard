cdc71b05ab0bbe974e61b1e3421caa1478787c90
unknown <webxite23@gmail.com>
2020-10-02 21:33:50 +0300

    changed picture url

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
-const Poke_Api =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
-
+const Poke_Api = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+ let padTo = (number) => 
+ number <= 999 ? `00${number}`.slice(-3) : number;
+;
 class Pokecard extends Component {
   render() {
     const { id, name, type, base_experience } = this.props;
-    let imgSrc = `${Poke_Api}${id}.png`;
+    let imgSrc = `${Poke_Api}${padTo(id)}.png`;
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-tile">{name}</h1>
